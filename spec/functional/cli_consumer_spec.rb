571ad74dbb1276e85c0c24832fb1551aed6e0ef5
Beth <beth@bethesque.com>
2015-04-25 20:47:09 +1000

    Remove redundant expectations (checking the file contents also checks that the file exists). Make the expectations the bear minimum to ensure that all the parts in the program fit together correctly. Make this a test that checks that the arguments are respected, but that won't need to change if the content of the files change (that is covered by the unit tests, no need to duplicate it).

--- spec/functional/cli_consumer_spec.rb
+++ spec/functional/cli_consumer_spec.rb
@@ -3,33 +3,40 @@ require 'pact/init/consumer'
 
 describe 'The pact-init-consumer command line interface' do
 
+  let(:pact_helper_path) { 'tmp/spec/service_providers/pact_helper.rb' }
+  let(:pact_helper_content) { File.read(pact_helper_path) }
+
   before do
     FileUtils.rm_rf('tmp')
     FileUtils.mkdir_p('tmp')
   end
 
-  context 'no arguments' do
+  context 'with no arguments' do
 
-    it 'creates the desired files and folder structure' do
+    before do
       Dir.chdir('tmp') do
         %x(bundle exec ../bin/pact-init-consumer)
-        expect(Dir.exists?('spec/service_providers')).to eq(true)
-        expect(File.exists?('spec/service_providers/pact_helper.rb')).to eq(true)
-        expect(File.exists?('spec/service_providers/pact_helper.rb')).to eq(true)
       end
+    end
+
+    it 'creates the desired files and folder structure' do
+      expect(pact_helper_content).to match("Pact.service_consumer")
     end
 
   end
 
   context 'with consumer and provider argument' do
 
-    it 'creates the desired files and folder structure' do
+    before do
       Dir.chdir('tmp') do
         %x(bundle exec ../bin/pact-init-consumer --consumer \" Foo Consumer\" --provider \" Bar Provider \")
-        expect(Dir.exists?('spec/service_providers')).to eq(true)
-        expect(File.exists?('spec/service_providers/pact_helper.rb')).to eq(true)
-        expect(File.exists?('spec/service_providers/pact_helper.rb')).to eq(true)
       end
+    end
+
+    it 'creates the desired files and folder structure' do
+      expect(pact_helper_content).to match("Pact.service_consumer")
+      expect(pact_helper_content).to match("Foo Consumer")
+      expect(pact_helper_content).to match("Bar Provider")
     end
 
   end
